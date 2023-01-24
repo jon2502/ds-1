cab701b5be49c697c1c15ac9df3946b60e3fb43f
jon2502 <jonbjarke25@outlook.dk>
2023-01-24 13:33:32 +0100

    opdatering af opgave1

--- opgave 1.4 del 1/del_1.js
+++ opgave 1.4 del 1/del_1.js
@@ -4,6 +4,22 @@ var encryptPrint = document.getElementById('encryptPrint')
 var decryptText = document.getElementById('decryptText');
 var decryptButton = document.getElementById('decryptButton')
 var decryptPrint = document.getElementById('decryptPrint')
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
-var new_alphabet = "";
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var result = "test";
+console.log(alphabet)
 
+encryptButton.addEventListener('click', encrypt)
+
+function encrypt () {
+    
+    for (let i = 0; i<encryptText.length; i++ )
+
+    encryptPrint.innerHTML = `${result}`
+}
+
+decryptButton.addEventListener('click', decrypt)
+
+function decrypt () {
+    for (let i = 0; i<decryptText.length; i++ )
+    decryptPrint.innerHTML=`${result}`
+}
