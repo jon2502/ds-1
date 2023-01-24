e8f6c271b6426d5ee331fcc99d206878ad2034f7
jon2502 <jonbjarke25@outlook.dk>
2023-01-24 16:42:38 +0100

    opdate

--- opgave 1.4 del 1/del_1.js
+++ opgave 1.4 del 1/del_1.js
@@ -5,21 +5,28 @@ var decryptText = document.getElementById('decryptText');
 var decryptButton = document.getElementById('decryptButton')
 var decryptPrint = document.getElementById('decryptPrint')
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var result = "test";
+var result = "";
 console.log(alphabet)
 
 encryptButton.addEventListener('click', encrypt)
 
 function encrypt () {
     
-    for (let i = 0; i<encryptText.length; i++ )
-
+    for (let i = 0; i<encryptText.length; i++ ){
+        var arrayEncrypt = Array.from(encryptText)
+        arrayEncrypt.split('')
+    }
+    console.log(arrayEncrypt.split(''))
+    result = encryptText.value
     encryptPrint.innerHTML = `${result}`
 }
 
 decryptButton.addEventListener('click', decrypt)
 
 function decrypt () {
-    for (let i = 0; i<decryptText.length; i++ )
+    for (let i = 0; i<decryptText.length; i++ ){
+        
+    }
+    result = decryptText.value
     decryptPrint.innerHTML=`${result}`
 }
