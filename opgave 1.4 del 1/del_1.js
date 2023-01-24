f3898d16fa12c59488d013f1be5023a2c2305e88
jon2502 <jonbjarke25@outlook.dk>
2023-01-24 12:20:47 +0100

    reset

--- opgave 1.4 del 1/del_1.js
+++ opgave 1.4 del 1/del_1.js
@@ -1,1 +1,9 @@
+var encryptText = document.getElementById('encryptText');
+var encryptButton = document.getElementById('encryptButton')
+var encryptPrint = document.getElementById('encryptPrint')
+var decryptText = document.getElementById('decryptText');
+var decryptButton = document.getElementById('decryptButton')
+var decryptPrint = document.getElementById('decryptPrint')
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var new_alphabet = "";
+
